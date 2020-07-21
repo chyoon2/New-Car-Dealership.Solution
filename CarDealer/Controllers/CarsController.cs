c9e9183c050a44b7cae332ab645647adebbab264
Chris Yoon <chy.yoon@gmail.com>
2020-07-21 11:57:07 -0700

    seperated logic
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

---
+++ CarDealer/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using CarDealer.Models;
+using System.Collections.Generic;
+
+namespace CarDealer.Controllers
+{
+  public class CarsController : Controller
+  {
+    [HttpGet("/cars")]
+    public ActionResult Index()
+    {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+    }
+
+    [HttpGet("/addcarform")]
+    public ActionResult AddCarForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/addcarform")]
+    public ActionResult AddCarForm(string makeModel, string miles, string price)
+    {
+      int milesInput = int.Parse(miles);
+      int priceInput = int.Parse(price);
+      Car myCar = new Car(makeModel, priceInput, milesInput);
+      return RedirectToAction("Index");
+    }
+  }
+}
--- CarDealer/Controllers/HomeController.cs
+++ CarDealer/Controllers/HomeController.cs
@@ -1,31 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using CarDealer.Models;
-using System.Collections.Generic;
 
 namespace CarDealer.Controllers
 {
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      List<Car> allCars = Car.GetAll();
-      return View(allCars);
-    }
 
-    [HttpGet("/addcarform")]
-    public ActionResult AddCarForm()
-    {
-      return View();
-    }
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
 
-    [HttpPost("/addcarform")]
-    public ActionResult AddCarForm(string makeModel, string miles, string price)
-    {
-      int milesInput = int.Parse(miles);
-      int priceInput = int.Parse(price);
-      Car myCar = new Car(makeModel, priceInput, milesInput);
-      return RedirectToAction("Index");
     }
-  }
 }
