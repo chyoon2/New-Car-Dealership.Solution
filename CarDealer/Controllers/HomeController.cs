da0a4dff96f76dc7548fd541475d62475358d620
Chris Yoon <chy.yoon@gmail.com>
2020-07-21 11:42:14 -0700

    added routes
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- CarDealer/Controllers/HomeController.cs
+++ CarDealer/Controllers/HomeController.cs
@@ -1,7 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+using CarDealer.Models;
 using System.Collections.Generic;
 
 namespace CarDealer.Controllers
 {
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+    }
+
+    [HttpGet("/addcarform")]
+    public ActionResult AddCarForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/addcarform")]
+    public ActionResult AddCarForm(string makeModel, string miles, string price)
+    {
+      int milesInput = int.Parse(miles);
+      int priceInput = int.Parse(price);
+      Car myCar = new Car(makeModel, priceInput, milesInput);
+      return RedirectToAction("Index");
+    }
+  }
 }
