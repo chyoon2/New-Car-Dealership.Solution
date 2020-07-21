20d703f0104749bee813cd153f7bf6104dd1bd0f
Chris Yoon <chy.yoon@gmail.com>
2020-07-21 11:17:35 -0700

    add a addcarform and instance static property
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- CarDealer/Models/Car.cs
+++ CarDealer/Models/Car.cs
@@ -2,21 +2,52 @@ using System;
 using System.Collections.Generic;
 
 namespace CarDealer.Models {
+  
   public class Car
   {
     private string _makeModel;
     private int _price;
     private int _miles;
+    private static List<Car> _instances = new List<Car> {};
+
     public void SetPrice(int newPrice)
     {
       _price = newPrice;
     }
+
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
+
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+
     public Car(string makeModel, int price, int miles)
     {
       _makeModel = makeModel;
       _price = price;
       _miles = miles;
+       _instances.Add(this);
     }
+
+    public static List<Car> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    
     public static string MakeSound(string sound)
     {
       return "Our cars sound like " + sound;
@@ -34,24 +65,9 @@ namespace CarDealer.Models {
       }
     }
 
-    public int GetPrice()
-    {
-      return _price;
-    }
-
-    public int GetMiles()
-    {
-      return _miles;
-    }
-
     public bool WorthBuying(int maxPrice)
     {
       return (_price < maxPrice);
     }
-      public string GetMakeModel()
-    {
-      return _makeModel;
-    }
   }
-
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+specs
+
+behavior: Add car to dealership site
+input: "Camry"
+output: *Camry added to web interface
+
+// creates a car obj, 
+//should
+we
